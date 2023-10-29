42c217c464a34afe193cdac75d44124e38fcc096
Green Gecko <34012548+greengeckowizard@users.noreply.github.com>
2023-10-28 22:54:22 -0500

    Add Cool Home Animation

--- adhdpanel/src/components/Home.js
+++ adhdpanel/src/components/Home.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeIn(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      style={{
+        opacity: fadeIn ? 1 : 0,
+        transition: "opacity 0.5s",
+        textAlign: "center",
+      }}
+    >
+      <h2
+        style={{
+          fontSize: "3rem",
+          marginBottom: "2rem",
+          transform: `translateY(${fadeIn ? "0" : "-50px"})`,
+          opacity: fadeIn ? 1 : 0,
+          transition: "transform 0.5s, opacity 0.5s",
+          transitionDelay: "0.2s",
+        }}
+      >
+        Welcome to the ADHD Panel
+      </h2>
+      <p
+        style={{
+          fontSize: "1.5rem",
+          transform: `translateY(${fadeIn ? "0" : "50px"})`,
+          opacity: fadeIn ? 1 : 0,
+          transition: "transform 0.5s, opacity 0.5s",
+          transitionDelay: "0.4s",
+        }}
+      >
+        We're here to help you manage your ADHD and improve your daily life.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
