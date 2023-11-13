bc31c27f35a18ad5f66e2390abbba9ef375f7169
Green Gecko <34012548+greengeckowizard@users.noreply.github.com>
2023-11-12 20:48:32 -0600

    Auth Revert

--- adhdpanel/backend-adhdpanel/main.js
+++ adhdpanel/backend-adhdpanel/main.js
@@ -3,39 +3,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
-const jwt = require("jsonwebtoken");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.status(401).send("Unauthorized");
-  }
-
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    res.status(401).send("Invalid token");
-  }
-};
-
-
 app.use(cors());
 app.use(bodyParser.json());
-app.use(verifyToken);
 
 const taskListFilePath = path.join(__dirname, "taskList.json");
-const userFilePath = path.join(__dirname, "users.json");
 
 let taskList = [];
-let users = [];
-
-const secretKey = "YOUR_SECRET_KEY"
 
 const saveTaskListToFile = () => {
   fs.writeFileSync(taskListFilePath, JSON.stringify(taskList), (err) => {
@@ -54,34 +31,7 @@ const loadTaskListFromFile = () => {
   }
 };
 
-const saveUsersToFile = () => {
-  fs.writeFileSync(userFilePath, JSON.stringify(users), (err) => {
-    if (err) {
-      console.error("Error writing users to file:", err);
-    }
-  });
-};
-
-const loadUsersFromFile = () => {
-  try {
-    const data = fs.readFileSync(userFilePath);
-    users = JSON.parse(data.toString());
-  } catch (err) {
-    console.error("Error loading users from file:", err);
-  }
-};
-
 loadTaskListFromFile();
-loadUsersFromFile();
-
-const authenticateUser = (username, password) => {
-  const user = users.find(
-    (user) => user.username === username && user.password === password
-  );
-  return user !== undefined;
-};
-
-
 
 app.get("/api/tasks", (req, res) => {
   res.json(taskList);
@@ -112,8 +62,8 @@ app.delete("/api/tasks/:index", (req, res) => {
 app.put("/api/tasks/complete-all", (req, res) => {
   taskList = taskList.map((taskItem) =>
     taskItem.status === TaskStatus.COMPLETED
-      ? taskItem
-      : { ...taskItem, status: TaskStatus.COMPLETED }
+    ? taskItem
+    : { ...taskItem, status: TaskStatus.COMPLETED }
   );
   saveTaskListToFile();
   res.sendStatus(200);
@@ -122,8 +72,8 @@ app.put("/api/tasks/complete-all", (req, res) => {
 app.put("/api/tasks/complete-pending", (req, res) => {
   taskList = taskList.map((taskItem) =>
     taskItem.status === TaskStatus.PENDING
-      ? { ...taskItem, status: TaskStatus.COMPLETED }
-      : taskItem
+    ? { ...taskItem, status: TaskStatus.COMPLETED }
+    : taskItem
   );
   saveTaskListToFile();
   res.sendStatus(200);
@@ -133,29 +83,6 @@ app.delete("/api/tasks", (req, res) => {
   taskList = [];
   saveTaskListToFile();
   res.sendStatus(200);
-});
-
-app.post("/api/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  if (authenticateUser(username, password)) {
-    const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
-    res.json({ token });
-  } else {
-    res.status(401).send("Invalid username or password");
-  }
-});
-
-app.post("/api/signup", async (req, res) => {
-  const { username, password } = req.body;
-  const userExists = users.some((user) => user.username === username);
-
-  if (userExists) {
-    res.status(409).send("User already exists");
-  } else {
-    const newUser = createUser(username, password);
-    res.sendStatus(201);
-  }
 });
 
 app.listen(port, () => {
--- adhdpanel/src/ADHDPanel.js
+++ adhdpanel/src/ADHDPanel.js
@@ -1,71 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useContext } from 'react';
-import { Navigate, BrowserRouter as Router, Link, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Tasks from './components/Tasks';
-import Timer from './components/Timer';
-import Reports from './components/Reports';
-import Calender from './components/Calender.tsx';
-import LoginForm from './components/Login';
-import './ADHDPanel.css';
-
-const AuthContext = React.createContext({
-  jwtToken: '',
-});
-
-const AuthProvider = ({ children }) => {
-  const [jwtToken, setJwtToken] = useState('token');
-
-  const login = async (username, password) => {
-    const response = await axios.post('/api/login', {
-      username,
-      password,
-    });
-
-    if (response.status === 200) {
-      setJwtToken(response.headers['authorization']);
-    }
-  };
-
-  const logout = async () => {
-    const response = await axios.post('/api/logout');
-
-    if (response.status === 200) {
-      setJwtToken('');
-    }
-  };
-
-  const value = {
-    jwtToken,
-    login,
-    logout,
-  };
-
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-const PrivateRoute = ({ children }) => {
-  const { jwtToken } = useContext(AuthContext);
-
-  if (!jwtToken) {
-    return <Navigate to="/login" />;
-  }
-
-  return <>{children}</>;
-};
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Tasks from "./components/Tasks";
+import Timer from "./components/Timer";
+import Reports from "./components/Reports";
+import Calender from "./components/Calender.tsx";
+import "./ADHDPanel.css";
 
 const ADHDPanel = () => {
-  const { jwtToken } = useContext(AuthContext);
-
-  if (!jwtToken) {
-    return <LoginForm />;
-  }
-
   return (
     <Router>
       <div className="adhd-panel">
@@ -84,50 +26,17 @@ const ADHDPanel = () => {
               <Link to="/reports">Reports</Link>
             </li>
             <li>
-              <Link to="/calendar">Calendar</Link>
-            </li>
-            <li>
-              <Link to="/logout">Logout</Link>
+              <Link to="/calendar">Calender</Link>
             </li>
           </ul>
         </div>
         <div className="content">
           <Routes>
-            <Routes path="/" element={<Home />} />
-            <Routes
-              path="/tasks"
-              element={
-                <PrivateRoute>
-                  <Tasks />
-                </PrivateRoute>
-              }
-            />
-            <Routes
-              path="/timer"
-              element={
-                <PrivateRoute>
-                  <Timer />
-                </PrivateRoute>
-              }
-            />
-            <Routes
-              path="/reports"
-              element={
-                <PrivateRoute>
-                  <Reports />
-                </PrivateRoute>
-              }
-            />
-            <Routes
-              path="/calendar"
-              element={
-                <PrivateRoute>
-                  <Calender />
-                </PrivateRoute>
-              }
-            />
-            <Routes path="/login" element={<LoginForm />} />
-            <Routes path="/logout" element={<LoginForm />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/timer" element={<Timer />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/calendar" element={<Calender />} />
           </Routes>
         </div>
       </div>
