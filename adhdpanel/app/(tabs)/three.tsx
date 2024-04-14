481debda4dc123970d5bb6e6812f4eee95556126
Birdup <34012548+birdup000@users.noreply.github.com>
2024-04-13 20:15:53 -0500

    fix subtasks adding to taskmap

--- adhdpanel/app/(tabs)/three.tsx
+++ adhdpanel/app/(tabs)/three.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Graph from "react-graph-vis";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 const TaskMap = () => {
   const [tasks, setTasks] = useState([]);
 
@@ -20,18 +19,26 @@ const TaskMap = () => {
   }, []);
 
   const graph = {
-    nodes: tasks.map((task) => ({
-      id: task.id,
-      label: task.text,
-      color: "#e04141",
-    })),
-    edges: tasks.flatMap((task, index) =>
-      task.subtasks?.map((subtask) => ({
+    nodes: tasks.flatMap((task) => [
+      { id: task.id, label: task.text, color: "#e04141" },
+      ...task.subtasks?.map((subtask) => ({
+        id: subtask.id,
+        label: subtask.text,
+        color: "#7be141",
+      })),
+    ]),
+    edges: tasks.flatMap((task, index) => [
+      ...task.subtasks?.map((subtask) => ({
         from: task.id,
         to: subtask.id,
-      }))
-          color: "lightblue",
-    ),
+        color: "lightblue",
+      })),
+      ...task.subtasks?.map((subtask) => ({
+        from: subtask.id,
+        to: task.id,
+        color: "lightblue",
+      })),
+    ]),
   };
 
   const options = {
